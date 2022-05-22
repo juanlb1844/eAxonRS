9b4071e9395d81b6a7e9044c323e349409bce8b4
juanLB <juanlb1844@gmail.com>
2022-05-22 17:22:58 -0500

    try

--- eaxon/resources/views/admin/editHotel.blade.php
+++ eaxon/resources/views/admin/editHotel.blade.php
@@ -139,8 +139,8 @@
                 r = JSON.parse(r); 
                 console.log( r ); 
                 url_image = r.link; 
-                $('.url_image').val(r.link); 
-                $('#img-prev').attr("src", r.link); 
+                $('.url_image').val(r.link);  
+                $('.prev-image').attr("background-image", "url('".r.link."'')"); 
                 /*  
                 $('.content-gallery-1').append('<div class="element-gallery imagen-'+r.id+' col-lg-2" link="'+r.link+'" style="background-image: url('+r.link+');"><input type="checkbox" class="status-photo" checked="" idphoto="1" /><button onclick="borrarFoto('+r.id+')" class="btn btn-default pull-right delete-img">&nbsp;</button></div>'); */
               } 
