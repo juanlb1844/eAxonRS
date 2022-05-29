77ffec99b8706e2afc2b4a93e617ecb75b8e2c06
juanLB <juanlb1844@gmail.com>
2022-05-29 02:21:08 -0500

    camera

--- eaxon/resources/views/client.blade.php
+++ eaxon/resources/views/client.blade.php
@@ -133,10 +133,8 @@
                 lastResult = decodedText;
                 // Handle on success condition with the decoded message.
                 console.log(`Scan result ${decodedText}`, decodedResult);
-
-
-
-                alert(decodedText); 
+ 
+                window.location.href = decodedText; 
             }
         }
 
