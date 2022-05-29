ff39ddd34f8646de4ef61d8ed7cc42c127d6ec90
juanLB <juanlb1844@gmail.com>
2022-05-29 01:04:47 -0500

    camera

--- eaxon/resources/views/client.blade.php
+++ eaxon/resources/views/client.blade.php
@@ -116,13 +116,16 @@
                 lastResult = decodedText;
                 // Handle on success condition with the decoded message.
                 console.log(`Scan result ${decodedText}`, decodedResult);
+
+
+
                 alert(decodedText); 
             }
         }
 
         var html5QrcodeScanner = new Html5QrcodeScanner(
             "qr-reader", { fps: 10, qrbox: 250, disableFlip : false});
-        html5QrcodeScanner.render(onScanSuccess);
+        html5QrcodeScanner.render({ facingMode: { exact: "environment" } }, onScanSuccess);
     });
 </script>
 
