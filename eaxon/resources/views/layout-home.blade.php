2adca4cfd3bbdd361dba2a4f5298b9ff435e2df8
juanLB <juanlb1844@gmail.com>
2022-07-09 11:58:54 -0500

    ajustes

--- eaxon/resources/views/layout-home.blade.php
+++ eaxon/resources/views/layout-home.blade.php
@@ -303,7 +303,7 @@
 		    m = (m < 10) ? "0" + m : m;
 		    s = (s < 10) ? "0" + s : s;
 		    
-		    var time = h + ":" + m + ":" + s + " " + session;
+		    var time = h + ":" + m + " " + session;
 		    document.getElementById("MyClockDisplay").innerText = time;
 		    document.getElementById("MyClockDisplay").textContent = time;
 		    
