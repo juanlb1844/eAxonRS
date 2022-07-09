970d9df6dd12c5201bc64f3e219ebc0c1d4fa287
juanLB <juanlb1844@gmail.com>
2022-07-09 11:31:21 -0500

    ajustes

--- eaxon/resources/views/layout-home.blade.php
+++ eaxon/resources/views/layout-home.blade.php
@@ -184,7 +184,7 @@
 			<!-- <span>{{$hash}}</span> --> 
 			<div class="col-lg-6 col-xs-3">
 				<a href='{{asset("/client/$hash/perfil/1")}}'>
-					<span class="header-title pull-left">eAxón	</span>
+					<span class="header-title pull-left">eAxón.</span>
 				</a> 
 			</div>
 			<div class="col-lg-6 col-xs-6" style="padding-top: 5px;">
